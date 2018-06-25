a67e475f6531fd78c693c72d5d27c5fda8206a2c
Aniruddha Bhamare <bhamare.anirudha@gmail.com>
2018-06-25 03:33:25 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+var express = require('express');
+
+var app = express();
+var hbs = require('hbs');
+var fs = require('fs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.set('view engin','hbs');
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+  res.render('maintanance.hbs')
+})
+
+app.use(express.static(__dirname + '/public'));
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now} ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFileSync('server.log',log + '\n' );
+next();
+});
+
+
+app.get('/',(req,res)=>{
+  res.render('index.hbs',{
+    pageTitle:'Welcome to My Website'
+  });
+})
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle : 'About Page'
+  });
+});
+
+
+//
+//  app.get('/',(req,res)=>{
+//    //res.send("Hello Express !");
+//    res.send('<h1>Hellow Express !<h1>')
+//  });
+
+//
+
+// app.get('/about',(req,res)=>{
+//     res.send({
+//       name:'Aniruddh',
+//       likes:[
+//         'cricket',
+//         'pc'
+//       ]
+//     });
+// });
+
+//
+// app.get('/about',(req,res)=>{
+//   res.send('<h2>About page !</h2>');
+// });
+
+// app.get('/bad',(req,res)=>{
+//   res.send({
+//     error:'Error Handling request'
+//   });
+// });
+
+ app.listen(3000,()=>{
+   console.log("Server is up on 3000 port");
+ });
